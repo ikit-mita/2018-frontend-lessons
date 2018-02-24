fb26c8b3879f80f5ddd583968a17fc1087246204
Denis Zavershinskiy <zaverden@gmail.com>
2018-02-24 12:59:10 +0700

    modules

--- 1-Intro/wwwroot/js/sw-view.js
+++ 1-Intro/wwwroot/js/sw-view.js
@@ -1,114 +1,33 @@
-﻿function setPerson(person) {
-    console.log(person);
-    ['name', 'hair_color', 'skin_color', 'eye_color', 'gender']
-        .forEach(field => {
-            const span = document.getElementById(field);
-            span.textContent = `${field}: ${person[field]}`
-        });
-    setCurrentPersonId(person.id);
-    const { hair_color, eye_color } = person;
-    updateHead(hair_color, eye_color);
-}
+﻿
+const loader = new window.Loader(
+    document.getElementsByClassName('loader')[0],
+    document.getElementsByClassName('head')[0],
+    document.getElementsByClassName('person-info')[0]
+);
 
-function updateHead(hair, eye) {
-    const head = document.getElementsByClassName('head')[0];
-    Array.from(head.classList)
-        .forEach(c => {
-            if (c.startsWith('hair') || c.startsWith('eye')) {
-                head.classList.remove(c);
-            }
-        });
-    head.classList.add(`hair-${hair}`);
-    head.classList.add(`eye-${eye}`);
-}
+const personService = new window.PersonService(loader);
 
-function loadPersonSync(id) {
-    startLoading();
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://swapi.co/api/people/${id}/`, false);
-    xhr.send();
-    stopLoading();
-    const person = JSON.parse(xhr.responseText);
-    person.id = id;
-    return person;
-}
-
-function loadPersonCallback(id, callback) {
-    startLoading();
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://swapi.co/api/people/${id}/`, true);
-
-    xhr.onload = () => {
-        stopLoading();
-        if (xhr.status !== 200) {
-            callback(new Error(`${xhr.status} ${xhr.statusText}`));
-        } else {
-            const person = JSON.parse(xhr.responseText);
-            person.id = id;
-            callback(null, person);
-        }
-    };
-
-    xhr.send();
-}
-
-function loadPersonPromise(id) {
-    startLoading();
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://swapi.co/api/people/${id}/`, true);
-    const promise = new Promise((resolve, reject) => {
-        xhr.onload = () => {
-            stopLoading();
-            if (xhr.status !== 200) {
-                reject(new Error(`${xhr.status} ${xhr.statusText}`));
-            } else {
-                const person = JSON.parse(xhr.responseText);
-                person.id = id;
-                resolve(person);
-            }
-        };
-    })
-    xhr.send();
-    return promise;
-}
-
-function getCurrentPersonId() {
-    const info = document.getElementsByClassName('person-info')[0];
-    return +(info.dataset.personId || 0);
-}
-
-function setCurrentPersonId(personId) {
-    const info = document.getElementsByClassName('person-info')[0];
-    info.dataset.personId = personId;
-}
-
-function startLoading() {
-    _setLoading(true);
-}
-
-function stopLoading() {
-    _setLoading(false);
-}
-
-function _setLoading(isLoading) {
-    document.getElementsByClassName('loader')[0].hidden = !isLoading;
-    document.getElementsByClassName('head')[0].hidden = isLoading;
-    document.getElementsByClassName('person-info')[0].hidden = isLoading;
-}
+const personView = new window.PersonView(
+    document.getElementsByClassName('person-info')[0],
+    document.getElementsByClassName('head')[0]
+);
 
 document.getElementById('next').addEventListener('click', ($event) => {
-    loadPersonCallback(getCurrentPersonId() + 1, (err, person) => {
+    personService.loadByIdCallback(personView.currentPersonId + 1, (err, person) => {
         if (err) {
             alert(err.message);
-            return;
+            throw err;
         }
-        setPerson(person);
+        personView.setPerson(person);
     });
 });
 
 document.getElementById('prev').addEventListener('click', ($event) => {
-    loadPersonPromise(getCurrentPersonId() - 1)
-        .then(setPerson)
-        .catch(err => alert(err.message));
+    personService.loadByIdPromise(personView.currentPersonId - 1)
+        .then(personView.setPerson.bind(personView))
+        .catch(err => {
+            alert(err.message);
+            throw err;
+        });
 });
 
